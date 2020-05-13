d5fa3bb7242035cb3cadae6235fcd713783019a6
Charles  Gray <electronicartist14@gmail.com>
2020-05-13 16:49:30 -0400

    Update Portal.java

--- Portal.java
+++ Portal.java
@@ -19,48 +19,92 @@ import java.lang.NullPointerException;
 */
 
 public class Portal{
-//The default constructor, which is left empty on purpose.
-public Portal(){}
+    
+    private String portal;
+    //The default constructor, which is left empty on purpose.
+    public Portal(){
+        portal = getUrlInfo();
+    }
+    
+    public String getPortal(){
+        return this.portal;
+    }
     
     public String getUrlInfo(){
 
+ 
+
         String urlString="http://happy-pets-web-1522142859.us-east-1.elb.amazonaws.com/patient-portal";
+
+ 
 
         String results=" ";
 
+ 
+
                try{
+
+ 
 
                    URL url=new URL(urlString);
 
+ 
+
                    URLConnection connect=url.openConnection();
+
+ 
 
                    BufferedReader rdr=new BufferedReader(new InputStreamReader(connect.getInputStream()));
 
+ 
+
                    results=rdr.readLine();
+
+ 
 
                    String nextLn=" ";
 
+ 
+
                    try {
+
+ 
 
                        while(nextLn.isEmpty()==false){
 
+ 
+
                            results=results+nextLn;
+
+ 
 
                            nextLn=rdr.readLine();
                        }
                    }
 
+ 
+
                    catch(NullPointerException e) {
 
+ 
+
                    }
+
+ 
 
                    rdr.close();
                }
 
+ 
+
                catch(IOException e){
+
+ 
 
                    return "F1";
                    }
+
+ 
 
         return results;
     }
