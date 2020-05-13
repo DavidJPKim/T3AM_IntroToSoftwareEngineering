69c8139c4d9fe7eacec6fb6b5a92acd8acef9b0e
Charles  Gray <electronicartist14@gmail.com>
2020-05-13 14:03:11 -0400

    Update PatientBillTestRunner.java

--- PatientBillTestRunner.java
+++ PatientBillTestRunner.java
@@ -1,13 +1,19 @@
-/package de.vogella.junit.first;
+//package de.vogella.junit.first;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-
+/**
+ * PatientBillTestRunner runs PatientBillTest and sees if there were any problem where received when testing
+ * PatientBill using JUnit to get the amount of failures assuming there are any.
+ * <p>
+ *
+ * @return Whether or not any units tests failed.
+ */
 
 public class PatientBillTestRunner {
 	public static void main(String[] args) {
 		Result result = JUnitCore.runClasses(PatientBillTest.class);
-		
+
 			if (result.getFailures().size() > 0){
     		System.err.println("A unit test failed:");
         		for (Failure failure : result.getFailures()) {
