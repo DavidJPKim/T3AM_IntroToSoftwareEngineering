6dcb1960f9186e30df2a7e4a824d28c3fefa8326
Charles  Gray <electronicartist14@gmail.com>
2020-05-13 14:02:25 -0400

    Update PatientBillTest.java

--- PatientBillTest.java
+++ PatientBillTest.java
@@ -1,10 +1,19 @@
-//import org.junit.*;
+import org.junit.*;
 
 public class PatientBillTest {
+
+   /**
+    * PatientBillTest takes public String expected directly from the website and uses getPatientBill() to
+    * assign a CustomerID to test out setCustomerInfo() from PatientBill.
+    * <p>
+    * @param  public String expected: The String that contains the result the information from the website
+    * @param  private PatientBill getPatientBill(): The code to get PatientBill with a CustomerID
+    * @return patient_bill_retrieved() sees if the answer contains the expected outcome.
+    */
    
-  
-   
-   // "patientBilling": [ { "date": "2019-01-01", "amount": 100.0, "customer_id": 100003, "service": "Annual Checkup", "patient_id": 99989 }, { "date": "2020-01-29", "amount": 105.0, "customer_id":
+   public String expected = "\"patientBilling\": "+
+   "[ { \"date\": \"2019-01-01\", \"amount\": 100.0, \"customer_id\": 100003, \"service\": \"Annual Checkup\", \"patient_id\": 99989 },"+
+   " { \"date\": \"2020-01-29\", \"amount\": 105.0, \"customer_id\": 100003, \"service\": \"Annual Checkup\", \"patient_id\": 99989 },";
   
    private PatientBill getPatientBill(){
       String CustomerID = "100003";
@@ -15,7 +24,7 @@ public class PatientBillTest {
    public void patient_bill_retrieved() {
      PatientBill patientBill = this.getPatientBill();
 		String result = patientBill.setCustomerInfo();
-    	Assert.assertNotEquals(result, "" );
+    	result.contains(expected);
       
    
       
@@ -23,4 +32,4 @@ public class PatientBillTest {
    }
    
  
-}
+
