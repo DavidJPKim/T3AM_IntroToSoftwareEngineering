34b627a114289004c52fc3fd07d666222dbb6f01
nprinker98 <44656058+nprinker98@users.noreply.github.com>
2020-05-13 23:02:04 -0400

    Update HPMS.java

--- HPMS.java
+++ HPMS.java
@@ -8,21 +8,27 @@ public class HPMS {
      */
     private static int componentCode;                                                                                                            
     public static boolean isArgumentValid(String[]args) {
-        if(args[0].equals("--customer-id")&&(args[2].equals("--billing")||args[2].equals("--patient-portal"))) {
+    	//tests if given the correct amount of arguments
+    	if(args.length!=1&&args.length!=3)return false;
+    	//tests if the first arg is --customer-id and the 3rd arg is --billing or --patient-portal
+    	else if(args[0].equals("--customer-id")&&(args[2].equals("--billing")||args[2].equals("--patient-portal"))) {
             componentCode=1;
             return true;
         }
+        //tests if first arg is --billing or --patient-portal nad the 2nd arg is --customer-id and also that the array is of length 3
         else if(args.length==3&&((args[0].equals("--billing")||args[0].equals("--patient-portal"))&&args[1].equals("--customer-id"))) {
             componentCode=1;
             return true;
         }
+        //tests if the only arg is --billing or --patient-portal
         else if(args.length==1&&(args[0].equals("--billing")||args[0].equals("--patient-portal"))) {
             componentCode=0;
             return true;
         }
+        //if this is reached then the arguments are invalid
         else return false;
     }
-    //
+    //takes the component code and decides which class that needs an object made for and prints the wanted results
     public static void componentCaller(int componentCode,String[] args) {
         if(componentCode==0) {
         
@@ -39,13 +45,7 @@ public class HPMS {
             
         }
         else {
-            String id;
-            
-                if(args[0].equals("--customer-id")){
-                    id=args[1];
-                }
-                else id=args[2];
-            
+            String id=getId(args);
             if(args[0].equals("--customer-id")&&args[2].equals("--billing")||args[0].equals("--billing")&& args[1].equals("--customer-id")){
                 //billPatient
                 PatientBill pBill=new PatientBill(id);
@@ -61,6 +61,14 @@ public class HPMS {
             
         }
     }
+         //takes String[] args as a parameter and finds the customer id in the arguments and returns it as a string
+        public static String getId(String[] args){
+             if(args[0].equals("--customer-id"))return args[1];
+                else return args[2];
+            
+        }
+    
+   
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         if(!isArgumentValid(args)) {
@@ -68,10 +76,11 @@ public class HPMS {
             System.exit(1);
         }
         componentCaller(componentCode,args);
+    }
 
  
 
-    }
+}
 
  
 
